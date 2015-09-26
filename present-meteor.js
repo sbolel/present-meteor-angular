700658311e8a35a370663ffd5cd92951f8712394
Alex Kuntz <akuntz09@gmail.com>
2015-09-26 12:46:07 -0500

    Voting works

--- present-meteor.js
+++ present-meteor.js
@@ -1,15 +1,36 @@
 Slides = new Mongo.Collection("slides");
-Negatives = new Mongo.Collection("negatives");
-Neutrals = new Mongo.Collection("neutrals");
-Positives = new Mongo.Collection("positives"); 
+Votes = new Mongo.Collection("votes");
   	
 if (Meteor.isClient) {
+
+   var myVote = {value:0, id:''};
+   Meteor.startup(function(){
+	myVote.id = Votes.insert({value:"0"});
+	
+   });
   // counter starts at 0
   Template.body.helpers({
 	getSlides : function(){
 		var s = Slides.find({});
   		console.log(s.count());
-  		return s;}
+  		return s;},
+  	
+  	positiveVotes: function(){
+  		return Votes.find({value:"1"}).count();
+  	},
+   negativeVotes: function(){
+  		return Votes.find({value:"-1"}).count();
+  	},
+   neutralVotes: function(){
+  		return Votes.find({value:"0"}).count();
+  	},
+   totalVotes: function(){
+  		return Votes.find({}).count();
+  	},
+  		
+  	votes: function(){
+  		return Votes.find({});
+  	}
   });
   
   Template.body.events({
@@ -18,7 +39,6 @@ if (Meteor.isClient) {
       // Prevent default browser form submit
       event.preventDefault();
       // Get value from form element
-      console.log(event.target.title.value);
       var text = event.target.title.value;
       // Insert a task into the collection
       Slides.insert({content: text, number : Slides.find().count()+1});
@@ -27,9 +47,14 @@ if (Meteor.isClient) {
     },
     
     "change #points": function(event){
-    	console.log($("#points").val());
+    	var currentValue = $("#points").val();
+    	Votes.update(myVote.id,{$set: {value: currentValue}});
+    	
+    },
+    
+    "click .clear": function(){
+    	Meteor.call('removeAllVotes');
     }
-
   });
   
   Template.slide.events({
@@ -41,7 +66,14 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
   Meteor.startup(function () {
+  	return Meteor.methods({
+  		removeAllVotes: function(){
+  			return Votes.remove({});
+  			
+  		}
+  	});
     // code to run on server at startup
   });
 }
