83b2f12e3cdd0040fe3510933395c7aeda479136
Sinan Bolel <sinanbolel@gmail.com>
2015-09-27 06:51:09 -0500

    Refactored code. Removed unnecessary scripts, made classes.

--- present-meteor.js
+++ present-meteor.js
@@ -1,131 +1,174 @@
-Slides = new Mongo.Collection("slides");
-Votes = new Mongo.Collection("votes");
-  	
-if (Meteor.isClient) {
+SlidesCollection = new Mongo.Collection("slides");
+VotesCollection = new Mongo.Collection("votes");
 
-  var myVote = {value:0, id:''};
+var Vote = function(voteKey, voteValue){
+  var that = this;
+  this.id;
+  this.value;
+  return initialize(voteKey, voteValue);
+  function initialize(voteKey, voteValue){
+    console.log("initializing", voteKey, voteValue)
+    that.id = voteKey;
+    that.value = voteValue;
+    return that;
+  }
+};
+
+Vote.prototype.key = function(){
+  return this.id;
+};
+Vote.prototype.setKey = function(key){
+  return this.id = key;
+};
+Vote.prototype.val = function(){
+  return this.value;
+};
+Vote.prototype.set = function(value){
+  return this.value = value;
+};
+
+var Slide = function(arguments){
+  var that = this;
+  this.template;
+  this.templateUrl;
+  function initialize(arguments){
+    for(key in arguments){
+      console.log(key);
+    }
+    return that;
+  }
+  return initialize(arguments);
+};
+
+var VoteChart = function(type){
+  var _chart;
+  if (type==='pie') {
+    _chart = {
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false
+      },
+      title: {
+          text: "Is everybody understanding this?"
+      },
+      tooltip: {
+          pointFormat: "<b>{point.percentage:.1f}%</b>"
+      },
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: "pointer",
+              dataLabels: {
+                  enabled: true,
+                  format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+                  style: {
+                      color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || "black"
+                  },
+                  connectorColor: "silver"
+              }
+          }
+      },
+      series: [{
+          type: "pie",
+          name: "votes",
+          data: [
+              ["Yes",   positiveVotes()],
+              ["Maybe",  neutralVotes()],
+              ["No",   negativeVotes()]
+          ]
+      }]
+    };
+  } else if (type==='gauge'){
+    chart = {
+
+    };
+  }
+  this.val = function(){
+    return _chart;
+  }
+  return this.val();
+};
+
+if (Meteor.isClient){
+  var myVote;
   Meteor.startup(function(){
-	   myVote.id = Votes.insert({value:"0"});
+    var initialVoteValue = "0"
+    var initialVoteId = VotesCollection.insert({value: initialVoteValue});
+    myVote = new Vote(initialVoteId, initialVoteValue);
   });
-
-  function positiveVotes() {
-    return Votes.find({value:"1"}).count();
-  };
-
-  function negativeVotes(){
-    return Votes.find({value:"-1"}).count();
-  };
-
-  function neutralVotes(){
-    return Votes.find({value:"0"}).count();
-  };
-
-  function totalVotes(){
-    return Votes.find({}).count();
-  };
-
   Template.body.helpers({
   	getSlides : function(){
-  		var s = Slides.find({});
+  		var s = SlidesCollection.find({});
     	console.log(s.count());
     	return s;
     },
-    
-    positiveVotes: function() {
+    positiveVotes: function(){
       return positiveVotes();
     },
-    negativeVotes: function() {
+    negativeVotes: function(){
       return negativeVotes();
     },
-    neutralVotes: function() {
+    neutralVotes: function(){
       return neutralVotes();
     },
-  		
   	votes: function(){
-  		return Votes.find({});
+  		return VotesCollection.find({});
   	},
-
-    voteChart: function() {
-      return {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: "Is everybody understanding this?"
-        },
-        tooltip: {
-            pointFormat: '<b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    },
-                    connectorColor: 'silver'
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'votes',
-            data: [
-                ['Yes',   positiveVotes()],
-                ['Maybe',  neutralVotes()],
-                ['No',   negativeVotes()]
-            ]
-        }]
-      };
+    voteChart: function(){
+      return new VoteChart('pie');
     }
   });
-  
+  function addSlide(content){
+
+  }
   Template.body.events({
-    "submit .new-title": function (event) {
-    
-      // Prevent default browser form submit
+    "submit .new-slide": function (event){
       event.preventDefault();
-      // Get value from form element
-      var text = event.target.title.value;
-      // Insert a task into the collection
-      Slides.insert({content: text, number : Slides.find().count()+1});
-      // Clear form
-      event.target.title.value = "";
+      SlidesCollection.insert({content: event.target.slide.value});
+      event.target.slide.value = "";
     },
-    
     "change #points": function(event){
     	var currentValue = $("#points").val();
-    	Votes.update(myVote.id,{$set: {value: currentValue}});
-    	
+      myVote.set(currentValue)
+      VotesCollection.update(myVote.key(), {$set: {value: currentValue}});
+      console.log("Changed to ",currentValue);
+      console.debug(myVote);
     },
-    
-    "click .clear": function(){
-    	Meteor.call('removeAllVotes');
+    "click button.clear-votes": function(){
+    	Meteor.call("removeAllVotes");
+    },
+    "click button.add-slide": function(){
+
     }
   });
-  
   Template.slide.events({
   	"click .delete": function(){
-  		Slides.remove(this._id);
+  		SlidesCollection.remove(this._id);
   	}
   });
+  function positiveVotes() {
+    return VotesCollection.find({value:"1"}).count();
+  };
+  function negativeVotes(){
+    return VotesCollection.find({value:"-1"}).count();
+  };
+  function neutralVotes(){
+    return VotesCollection.find({value:"0"}).count();
+  };
+  function totalVotes(){
+    return VotesCollection.find({}).count();
+  };
 }
 
-if (Meteor.isServer) {
 
-  Meteor.startup(function () {
+if (Meteor.isServer){
+  Meteor.startup(function (){
   	return Meteor.methods({
   		removeAllVotes: function(){
-  			return Votes.remove({});
-  			
+  			return VotesCollection.remove({});
   		}
   	});
-    // code to run on server at startup
   });
 }
 
